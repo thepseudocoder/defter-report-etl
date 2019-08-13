04bb6a07dddb3cb1d9259c0b89ead37a9b92db7d
Burak <thepseudocoder@users.noreply.github.com>
2019-08-13 13:13:27 +0300

    initial etl processes list created.

--- dre.py
+++ dre.py
@@ -2,6 +2,34 @@ import xlrd
 import csv
 import io
 
+## ETL process list
+# The list of functions to be applied each line. Can be modified.
+
+processList = [illegalLine, addAccountName, absAmount, deleteSumCol]
+
+
+## ETL processes ##
+# Each process takes a csv line and returns a processes line to be
+# appended to the result file.
+
+# If a processed line's result is Null, processes return 0.
+
+# determine if the line is empty or a seperator line
+def illegalLine(line):
+        pass
+
+# adds account name to the first column
+def addAccountName(line):
+        pass
+
+# takes the absolute value of amount column 
+def absAmount(line):
+        pass
+
+# deletes the last column (total sum)
+def deleteSumCol(line):
+        pass
+
 # Take a workbook and return csv string
 def convertCSV(workbookpath):
 
@@ -15,3 +43,4 @@ def convertCSV(workbookpath):
             c.writerow(sh.row_values(r))
 
     return output.getvalue()
+
