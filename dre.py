691383f3daab40156214d5670066ffdc6e3abe88
Burak <thepseudocoder@users.noreply.github.com>
2019-08-14 17:34:48 +0300

    Update dre.py
    new etl process: convert dates

--- dre.py
+++ dre.py
@@ -1,6 +1,10 @@
 import xlrd
 import csv
 import io
+import sys
+import os
+import datetime
+from os import walk
 
 #### Some variables
 
@@ -32,6 +36,9 @@ def omitRow(*args):
 	# first 3 rows of the file are automatically omitted
 	if row_num < OmitFirstRows:
 		return 0
+	# if there are less than 6 cells, then its probably an empty row
+	elif len(cell_list) < 6:
+		return 0
 	# if there are more than 2 empty cells, then its probably a seperator row
 	elif empty_cell_count >= 2:
 		return 0
@@ -49,14 +56,14 @@ def addAccountName(*args):
 
 # takes the absolute value of amount column
 # note: changing the ProcessList may broke this function
-# since it exclusively takes the absoulte value of 4th cell
+# since it exclusively takes the absoulte value of 5th cell
 def absAmount(*args):
 	row = args[0]
 	cell_list = row.split(",")
 
-	# 4th cell contains the amount
-	cell_list[3] = int(cell_list[3])
-	cell_list[3] = abs(cell_list[3])
+	# 5th cell contains the amount
+	cell_list[4] = float(cell_list[4])
+	cell_list[4] = abs(cell_list[4])
 
 	row = ','.join(map(str,cell_list))
 
@@ -84,6 +91,23 @@ def deleteIfUnwatedCategory(*args):
 
 	return row
 
+# convert date integers to dates
+# note: changing the ProcessList may broke this function
+# since it exclusively converts the value of 0th cell
+def convertDates(*args):
+	row = args[0]
+	cell_list = row.split(",")
+
+	datefloat = float(cell_list[0])
+	adjusteddate = (datefloat - 25569) * 86400.0
+
+	resultdate = datetime.datetime.utcfromtimestamp(adjusteddate)
+	cell_list[0] = resultdate.strftime('%Y-%m-%d')
+
+	row = ','.join(map(str,cell_list))
+	
+	return row
+
 #######################
 
 # Take a workbook and return csv string
@@ -100,15 +124,66 @@ def convertCSV(workbookpath):
 
 	return output.getvalue()
 
+# Get account name from the file path
+def deriveAccountName(filePath):
+	return filePath.split(".")[0].split("\\")[-1]
+
 ## ETL process list
 # The list of functions to be applied each row. Can be modified.
 ProcessList = [
 	omitRow,
-	absAmount, 
+	absAmount,
+	convertDates, 
 	deleteSumCol,
 	addAccountName
 	]
 
 if __name__ == '__main__':
 	
-	pass
+	path = sys.argv[1]
+	filelist = []
+
+	# get only xlsx files
+	for (dirpath, dirnames, filenames) in walk(path):
+		for a_file in filenames:
+			if ".xlsx" in a_file:
+				filelist.append(os.path.join(dirpath, a_file))
+	
+	# extra check for temporary/hidden files
+	for item in filelist:
+		if "~$" in item or item.split("\\")[-1][0] == ".":
+			filelist.remove(item)
+
+	resultCSV = ""
+
+	for each_file in filelist:
+		output = convertCSV(each_file)
+		rows = output.split("\n")
+		
+		# account name is derived from the filename
+		accountname = deriveAccountName(each_file)
+		print("Processing account: " + accountname)
+		rowNum = 0
+
+		for row in rows:
+			processedRow = row
+
+			for func in ProcessList:
+				result = func(processedRow, rowNum, accountname)
+				
+				if result == 0:
+					processedRow = 0
+					break
+				else:
+					processedRow = result
+			
+			rowNum+=1
+
+			if processedRow == 0:
+				continue
+			else:
+				resultCSV += processedRow + "\n"
+	
+	with open('result.csv', 'w', newline="", encoding="utf-8") as f:
+		f.write(resultCSV)
+	
